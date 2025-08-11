d8a526c6b5930ffd3954102f58cdbaec636c0979
Filatov Anton <filatov@intaro.ru>
2025-08-11 22:01:30 +0300

    some refactor

--- web/components/EmptyTransactions.tsx
+++ web/components/EmptyTransactions.tsx
@@ -23,14 +23,13 @@ export default function EmptyTransactions({ onCreateClick }: EmptyTransactionsPr
         </p>
         
         <div className="flex flex-col sm:flex-row gap-3 justify-center">
-          <Link
-            href="/transactions/new"
+          <button
             onClick={onCreateClick}
             className="inline-flex items-center justify-center px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-lg hover:shadow-xl transition-all duration-200 transform hover:scale-105"
           >
             <Icon name="plus" size={18} className="mr-2" />
             Создать первую транзакцию
-          </Link>
+          </button>
           
           <Link
             href="/categories"
